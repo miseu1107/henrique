f6a87296d4a0037537cc1674fe743b77ff4a2371
João Henrique <miseu1107@gmail.com>
2018-02-27 20:47:09 -0300

    op logicos

---
+++ aula02/oplogicos.php
@@ -0,0 +1,36 @@
+<?php
+//OPERADORES logicos
+echo "<pre>";
+echo "<br>";
+echo "<hr>";
+
+
+var_dump(true and true);
+var_dump(true and false);
+var_dump(true && true);
+var_dump(true && false);
+
+echo "<br>";
+echo "<hr>";
+
+var_dump(true or true);
+var_dump(true or false);
+var_dump(true || true);
+var_dump(true|| false);
+var_dump(false || false);
+
+echo "<br>";
+echo "<hr>";
+
+var_dump(true xor true);
+var_dump(true xor false);
+var_dump(false xor false);
+
+echo "<br>";
+echo "<hr>";
+
+var_dump(!true);
+var_dump(!false);
+
+
+
