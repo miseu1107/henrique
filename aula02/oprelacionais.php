716de7dca72e09f961c0ee500b010d50b90454ac
João Henrique <miseu1107@gmail.com>
2018-02-27 20:30:10 -0300

    op relacionais

---
+++ aula02/oprelacionais.php
@@ -0,0 +1,78 @@
+<?php
+//OPERADORES RELACIONAIS
+echo "<pre>";
+echo "<br>";
+echo "<hr>";
+
+
+//igual
+echo "igual";
+echo "<br>";
+var_dump(2==2);
+var_dump(2=='2');
+var_dump(2==3);
+echo "<br>";
+echo "<hr>";
+
+//identico
+echo "identico";
+echo "<br>";
+var_dump(2===2);
+var_dump(2==='2');
+echo "<br>";
+echo "<hr>";
+
+//diferente
+echo "diferente";
+echo "<br>";
+var_dump(2!=='2');
+var_dump(3!==2);
+echo "<br>";
+echo "<hr>";
+
+//menor que
+echo "menor que";
+echo "<br>";
+var_dump(2<5);
+var_dump(5<2);
+var_dump(2<2);
+echo "<br>";
+echo "<hr>";
+
+//menor ou igual
+echo "menor ou igual";
+echo "<br>";
+var_dump(2<=5);
+var_dump(5>=2);
+var_dump(2<=2);
+echo "<br>";
+echo "<hr>";
+
+//maior ou igual
+echo "maior ou igual";
+echo "<br>";
+var_dump(2>=5);
+var_dump(5>=2);
+var_dump(2<=2);
+echo "<br>";
+echo "<hr>";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
