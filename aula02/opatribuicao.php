044f973ea71fe4ba78aac50cc05ab7d9f20faf2f
João Henrique <miseu1107@gmail.com>
2018-02-27 20:00:09 -0300

    novo

--- aula02/opatribuicao.php
+++ aula02/opatribuicao.php
@@ -6,6 +6,7 @@ echo "<pre>";
 echo "<br>";
 echo "<hr>";
 
+
 $a=5;
 var_dump($a);
 $a += 5; // e a mesma coisa q $a =  $a+5;
@@ -16,6 +17,8 @@ $a *= 3;// e a mesma coisa q $a =  $a*3;
 var_dump($a);
 $a /= 4;// e a mesma coisa q $a =  $a/4;
 var_dump($a);
+
+
 
 echo "<br>";
 echo "<hr>";
