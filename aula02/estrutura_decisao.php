270120a9d4fc8a6c9237b7c31aa450cda84d20b7
João Henrique <miseu1107@gmail.com>
2018-02-27 21:39:58 -0300

    decisao_if

---
+++ aula02/estrutura_decisao.php
@@ -0,0 +1,19 @@
+<?php
+//string
+echo "<pre>";
+echo "<br>";
+echo "<hr>";
+
+$idade = 19;
+$carterinha = true;
+
+
+if (($idade >= 18)&& $carterinha){
+echo "maior idade e carterinha";
+ }else if (!$carterinha ===false){
+ 	echo "menor idade faça carterinha";
+
+
+
+
+ }
